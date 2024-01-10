342883040622ce8c70ffce88f38fbdb52a98adff
Jane Njeri Mwangi <janenjeri@Janes-MBP.lan>
2024-01-10 13:00:17 -0600

    Initial commit

---
+++ src/mos_quizzle.py
@@ -0,0 +1,18 @@
+#! usr/bin/python3 -i   # -i is for 'interactive' mode
+
+"""
+File: mos_quizzle.py
+A simple program that helps players learn about different mosquito species and their characteristics, 
+including biting behavior, and adaptation to the environment
+
+"""
+
+# import all necessary functions
+
+# launcher - launches the main file, including the mainmenu
+# mainmenu - shows different options for checking mosquito species and associated characteristics
+# module to check genus
+# module to show physical features (antennae, proboscis, head, thorax, legs)
+# module to show biting behaviour
+# module to show adaptation to different environments
+
