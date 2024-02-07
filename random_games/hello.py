5aa26e56d846d7b7d42e9092d1aaf18db1f4452d
JaneNjeri <aquilanjeri@gmail.com>
2024-02-07 20:16:43 +0300

    adding main_menu file

---
+++ random_games/hello.py
@@ -0,0 +1,11 @@
+#! usr/bin/python3
+
+# This program says hello and prints my name
+
+print("Hello world")
+print("What is your name?")
+
+myName = input()
+
+print('It is good to meet you, ' + myName)
+
